48c07b1ad2405fd039c96c8f7d1ea4bb6972ab66
Erwin0000Smith <153807532+Erwin0000Smith@users.noreply.github.com>
2024-08-08 18:52:01 +0700

    Point 9 & 10

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -9,14 +9,13 @@ const { message } = storeToRefs(store)
 <template>
   <div class="text-center font-sans text-gray-700 antialias">
     <header>
-      <div id="flashMessage" v-if="message">
-        <h4>{{ message }}</h4>
+      <div id="flashMessage" class="animate-fade" v-if="message">        <h4>{{ message }}</h4>
       </div>
       <div class="wrapper">
-        <nav>
-          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink>
-          | <RouterLink :to="{ name: 'about' }">About</RouterLink> |
-          <RouterLink :to="{ name: 'student' }">Student</RouterLink>
+        <nav class="py-6">
+          <RouterLink class="font-bold  text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view' }">Event</RouterLink>
+          | <RouterLink class="font-bold  text-gray-700" exact-active-class="text-green-500" :to="{ name: 'about' }">About</RouterLink> |
+          <RouterLink class="font-bold  text-gray-700" exact-active-class="text-green-500" :to="{ name: 'student' }">Student</RouterLink>
         </nav>
       </div>
     </header>
@@ -26,19 +25,19 @@ const { message } = storeToRefs(store)
 </template>
 
 <style>
-nav a {
+/* nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 nav a.router-link-exact-active {
   color: #42b983;
-}
-
+} */
+/* 
 h2 {
   font-size: 20px;
-}
+} */
 /* lab 4. 1.5 */
-@keyframes yellofade {
+/* @keyframes yellofade {
   from {
     background-color: yellow;
   }
@@ -48,5 +47,5 @@ h2 {
 }
 #flashMessage {
   animation: yellofade 3s ease-in-out;
-}
+} */
 </style>
