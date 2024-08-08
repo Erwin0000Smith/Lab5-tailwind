6865a9d61f605552b37b9b86c0869354036ee297
Erwin0000Smith <153807532+Erwin0000Smith@users.noreply.github.com>
2024-08-08 16:44:58 +0700

    Point 1-3

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -36,7 +36,7 @@
 <template>
   <h1>Events For Good</h1>
     <!-- new element-->
-  <div class="events">
+  <div class="flex flex-col items-center">
     <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
     <EventDetails v-for="event in events" :key="'details-' + event.id" :event="event"></EventDetails>
   <div class="pagination">
@@ -60,10 +60,8 @@
 </template>
 
 <style scoped>
-  .events {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  .nav {
+    padding: 30;
   }
   .pagination {
     display: flex;
